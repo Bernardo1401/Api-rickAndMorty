f0ac914f01e5baa7d3cd44594aedbe3ab526465f
Bernardo <sardinhaber@gmail.com>
2025-04-10 11:10:51 -0300

    feat: project

---
+++ app/home/page.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import styles from "./Home.module.css"
+import CharacterCard from "../../components/CharacterCard"
+import axios from "axios";
+
+export default function Home() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get('https://rickandmortyapi.com/api/character')
+        .then(response => {
+            setCharacters(response.data.results)
+        })
+        .catch ((error) => {
+            console.error('Erro ao buscar personagem: ', error)
+        });
+    },[]);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.grid} >
+            {characters.map((char) => (
+                <CharacterCard key={char.id} character={char} />
+            ))}
+            </div>
+        </div>
+    );
+}
